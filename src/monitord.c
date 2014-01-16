85e869eb6270e3c915b16ad7b76bc43dc47a2ac8
Phillip DIXON <phil@dixon.gen.nz>
2014-01-16 22:29:20 +1300

    Basic clamshell mode support.
    - Listen to upower-glib to see if we have ac power, and check the lid
      state.
    - Inhibit loginds handling of the lid switch when we have power and an
      external monitor.

--- src/monitord.c
+++ src/monitord.c
@@ -13,12 +13,15 @@
  *
  * Copyright (C) Phillip Dixon, 2014
  */
-
+#include <assert.h>
 #include <stdlib.h>
 #include <glib.h>
+#include <gio/gio.h>
+#include <gio/gunixfdlist.h>
 #include <xcb/xcb.h>
 #include <xcb/xcb_aux.h>
 #include <xcb/randr.h>
+#include <upower.h>
 
 #include "pd_xcb_source.h"
 #include "log.h"
@@ -31,52 +34,171 @@ static inline void pd_free(void *p)
 }
 
 typedef struct {
-    gboolean on_ac;
-    gboolean lid_closed;
-    gboolean external_display;
+    int lid_inhibit_fd;
+    uint8_t on_battery:1;
+    uint8_t lid_present:1;
+    uint8_t lid_closed:1;
+    uint8_t ext_display_present:1;
+    uint8_t ext_display_active:1;
+    uint8_t int_display_present:1;
+    uint8_t int_display_active:1;
 } SystemState;
 
 typedef struct {
     xcb_connection_t *c;
     int event_base;
-} RandrContext;
+    GDBusProxy *proxy;
+    SystemState state;
+} Context;
 
-void handle_output_change(xcb_connection_t *c, xcb_randr_notify_event_t *e)
+static void take_inhibit_cb(GObject *source_object,
+                            GAsyncResult *res,
+                            gpointer user_data)
+{
+    GVariant *result = NULL;
+    GError *error = NULL;
+    GUnixFDList *fd_list;
+    gint32 fd_index = 0;
+    Context *context = (Context *)user_data;
+
+    result = g_dbus_proxy_call_with_unix_fd_list_finish(context->proxy,
+                                                        &fd_list,
+                                                        res,
+                                                        &error);
+    if (!result) {
+        log_warn("Error taking lid inhibitor lock: %s", error->message);
+        g_error_free(error);
+        return;
+    }
+
+    g_variant_get(result, "(h)", &fd_index);
+    context->state.lid_inhibit_fd = g_unix_fd_list_get(fd_list, fd_index, &error);
+    if (context->state.lid_inhibit_fd == -1) {
+        log_warn("Error getting file descriptor for lid inhibitor lock: %s",
+                   error->message);
+        g_error_free(error);
+    }
+    g_variant_unref(result);
+    g_object_unref(fd_list);
+}
+
+static void take_inhibit(Context *context)
+{
+    if(context->state.lid_inhibit_fd >= 0)
+    {
+        return;
+    }
+    else
+    {
+        g_dbus_proxy_call_with_unix_fd_list(context->proxy,
+                                            "Inhibit",
+                                            g_variant_new("(ssss)",
+                                                          "handle-lid-switch",
+                                                          "monitord",
+                                                          "support clamshell.",
+                                                          "block"),
+                                            G_DBUS_CALL_FLAGS_NONE,
+                                            -1,
+                                            NULL,
+                                            NULL,
+                                            &take_inhibit_cb,
+                                            context);
+    }
+}
+
+static void release_inhibit(Context *context)
+{
+    if(context->state.lid_inhibit_fd >= 0)
+    {
+        close(context->state.lid_inhibit_fd);
+        context->state.lid_inhibit_fd = -1;
+        return;
+    }
+}
+
+void print_state(SystemState state)
+{
+    log_info("System state: bat: %d lid_inhibit: %d, lid_present: %d lid_closed: %d, ext_present: %d, ext_active: %d",
+             state.on_battery,
+             state.lid_inhibit_fd,
+             state.lid_present,
+             state.lid_closed,
+             state.ext_display_present,
+             state.ext_display_active);
+}
+
+void apply(Context *c)
+{
+    print_state(c->state);
+    if(!c->state.lid_closed &&
+       ((!c->state.ext_display_active && c->state.ext_display_present) ||
+        (!c->state.int_display_active && c->state.int_display_present)))
+    {
+        log_info("Go to Dualhead");
+        system("xrandr --output DVI1 --auto --above LVDS1 --output LVDS1 --auto");
+    }
+    else if (c->state.ext_display_active &&
+             !c->state.ext_display_present)
+    {
+        log_info("Go to Internal Only");
+        system("xrandr --output DVI1 --off --output LVDS1 --auto");
+    }
+    else if (c->state.ext_display_active &&
+             c->state.ext_display_present &&
+             c->state.lid_closed &&
+             c->state.lid_inhibit_fd >= 0)
+    {
+        log_info("Go to External Only");
+        system("xrandr --output LVDS1 --off");
+    }
+
+    if(!c->state.on_battery &
+       c->state.lid_present &
+       c->state.ext_display_present &
+       c->state.ext_display_active)
+    {
+        log_info("Mask lid");
+        take_inhibit(c);
+    } else {
+        log_info("Unmask lid");
+        release_inhibit(c);
+    }
+}
+
+void handle_output_change(Context *c, xcb_randr_notify_event_t *e)
 {
     xcb_randr_get_output_info_cookie_t info;
     xcb_randr_get_output_info_reply_t *reply;
     xcb_randr_output_change_t *change = &e->u.oc;
-    info = xcb_randr_get_output_info(c, change->output, 0);
-    reply = xcb_randr_get_output_info_reply(c, info, 0);
+    info = xcb_randr_get_output_info(c->c, change->output, 0);
+    reply = xcb_randr_get_output_info_reply(c->c, info, 0);
     cleanup_free const char *name = ({
             uint8_t *name = xcb_randr_get_output_info_name(reply);
             int len = xcb_randr_get_output_info_name_length(reply);
             strndup((char *)name, len);
         });
-    log_info("output %s connection: %d, crtc: %d",
-             name, change->connection, change->crtc);
     if(!strcmp(name, "DVI1")) {
-        if(!change->connection && !change->crtc) {
-            log_info("enabling DVI");
-            system("xrandr --output DVI1 --auto --above LVDS1");
-        } else if (change->connection && change->crtc) {
-            log_info("disabling DVI");
-            system("xrandr --output DVI1 --off");
-        }
+        c->state.ext_display_present = change->connection == 0 ? TRUE : FALSE;
+        c->state.ext_display_active = change->crtc ? TRUE : FALSE;
+    } else if (!strcmp(name, "LVDS1")) {
+        c->state.int_display_present = change->connection == 0 ? TRUE : FALSE;
+        c->state.int_display_active = change->crtc ? TRUE : FALSE;
     }
+    apply(c);
 }
 
 static gboolean handle_xcb_event(xcb_generic_event_t *event,
                                  gpointer user_data)
 {
-    RandrContext *context;
-    check(event, "No event");
-    check(user_data, "No user_data");
+    Context *context;
+    assert(event);
+    assert(user_data);
 
-    context = (RandrContext *)user_data;
+    context = (Context *)user_data;
     switch((event->response_type & ~0x80) - context->event_base) {
         case XCB_RANDR_NOTIFY_OUTPUT_CHANGE:
-            handle_output_change(context->c, (xcb_randr_notify_event_t *)event);
+            log_info("xcb output changed");
+            handle_output_change(context, (xcb_randr_notify_event_t *)event);
             break;
         default:
             log_info("Unknown xcb event.");
@@ -87,9 +209,28 @@ error:
     return FALSE;
 }
 
+static void on_upower_changed(UpClient *client,
+                              gpointer user_data)
+{
+    Context *context;
+
+    assert(client);
+    assert(user_data);
+
+    context = (Context *)user_data;
+
+    context->state.on_battery = up_client_get_on_battery(client);
+    context->state.lid_present = up_client_get_lid_is_present(client);
+    context->state.lid_closed = up_client_get_lid_is_closed(client);
+    apply(context);
+}
+
 int main(int argc, char *argv[])
 {
     GMainLoop *loop;
+
+    GError *error;
+
     PDXcbSource *xcb;
 
     cleanup_free xcb_connection_t *c;
@@ -100,8 +241,31 @@ int main(int argc, char *argv[])
     int screen;
     int r;
 
-    RandrContext context;
+    Context context;
 
+    UpClient *client;
+
+    loop = g_main_loop_new(NULL, FALSE);
+    check(loop, "Failed to create g_main_loop");
+
+    // Setup logind DBus proxy;
+    error = NULL;
+    context.proxy = g_dbus_proxy_new_for_bus_sync(G_BUS_TYPE_SYSTEM,
+                                                  G_DBUS_PROXY_FLAGS_NONE,
+                                                  NULL,
+                                                  "org.freedesktop.login1",
+                                                  "/org/freedesktop/login1",
+                                                  "org.freedesktop.login1.Manager",
+                                                  NULL,
+                                                  &error);
+    if(!context.proxy) {
+        log_err("Error creating DBus proxy: %s\n", error->message);
+        g_error_free(error);
+        goto error;
+    }
+    context.state.lid_inhibit_fd = -1;
+
+    // Setup xcb event monitoring
     c = xcb_connect(NULL, &screen);
     r = xcb_connection_has_error(c);
     check(r == 0, "xcb_connect failed with error %d", r);
@@ -114,9 +278,6 @@ int main(int argc, char *argv[])
     root = xcb_aux_get_screen(c, screen);
     check(root, "Failed to retrive X11 root");
 
-    loop = g_main_loop_new(NULL, FALSE);
-    check(loop, "Failed to create g_main_loop");
-
     xcb = pd_xcb_source_new(NULL,
                            c,
                            &handle_xcb_event,
@@ -126,6 +287,25 @@ int main(int argc, char *argv[])
 
     xcb_randr_select_input(c,root->root,XCB_RANDR_NOTIFY_MASK_OUTPUT_CHANGE);
     xcb_flush(c);
+
+    // Setup UPower signals
+    client = up_client_new();
+    check(client, "Failed to init UPower client");
+    g_signal_connect(client, "changed", G_CALLBACK(on_upower_changed), &context);
+
+    // Initialise to the current state
+    context.state.on_battery = up_client_get_on_battery(client);
+    context.state.lid_present = up_client_get_lid_is_present(client);
+    context.state.lid_closed = up_client_get_lid_is_closed(client);
+
+    // TODO probe display status at startup rather than assuming.
+    context.state.ext_display_present = FALSE;
+    context.state.ext_display_active = FALSE;
+
+    context.state.int_display_present = TRUE;
+    context.state.int_display_active = TRUE;
+
+    apply(&context);
 
     g_main_loop_run(loop);
 
